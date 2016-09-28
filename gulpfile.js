32e716185f0447fd1b805efc555727439c2dd7af
Šime Jerković <simejerkovic@users.noreply.github.com>
2016-09-28 16:22:38 +0200

    remove deprecated styles

--- gulpfile.js
+++ gulpfile.js
@@ -27,9 +27,7 @@ var autoprefixer = require('autoprefixer');
 
 // Local directory path
 // ==========================================================================
-var skinPathHNK = 'Portals/0-System/Skins/HNK'; // HNK Split
-var skinPathSL = 'Portals/2-System/Skins/62-Splitskoljeto'; // Splitsko ljeto
-var skinPathMD = 'Portals/3-System/Skins/27-MaruliceviDani'; // Marulićevi dani
+var skinPathHNK = 'Portals/0-System/Skins/HNK';
 
 /*******************************************************************************
     ERROR HANDLER FOR GULP PLUMBER
@@ -42,7 +40,6 @@ var onError = function (err) {
 /*******************************************************************************
  Compile Sass to CSS
 *******************************************************************************/
-// HNK Split
 gulp.task('styles:hnk', function () {
     var processors = [
         autoprefixer({browsers: ['> 1%', 'last 2 versions', 'ie > 6', 'ff ESR', 'bb >= 7', 'ios >= 7']}),
@@ -66,79 +63,13 @@ gulp.task('styles:hnk', function () {
         .pipe(browserSync.stream({ match: '**/*.css' }));
 });
 
-// Splitsko ljeto
-gulp.task('styles:sl', function () {
-    var processors = [
-        autoprefixer({browsers: ['> 1%', 'last 2 versions', 'ie > 6', 'ff ESR', 'bb >= 7', 'ios >= 7']}),
-        pxtorem({
-            root_value: 10,
-            replace: true,
-            prop_white_list: [],
-            selector_black_list: ['html']
-        })
-    ];
-
-    return gulp.src(skinPathSL + '/styles/skin.scss')
-        .pipe(plumber())
-        .pipe(sourcemaps.init())
-        .pipe(sass.sync().on('error', sass.logError))
-        .pipe(postcss(processors)) // px-to-rem & autoprefixer
-        .pipe(nano({ safe: true, discardComments: { removeAll: false }, zindex: false }))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(skinPathSL))
-        .pipe(browserSync.stream({ match: '**/*.css' }));
-});
-
-// Marulicevi dani
-gulp.task('styles:md', function () {
-    var processors = [
-        autoprefixer({browsers: ['> 1%', 'last 2 versions', 'ie > 6', 'ff ESR', 'bb >= 7', 'ios >= 7']}),
-        pxtorem({
-            root_value: 10,
-            replace: true,
-            prop_white_list: [],
-            selector_black_list: ['html']
-        })
-    ];
-
-    return gulp.src(skinPathMD + '/styles/skin.scss')
-        .pipe(plumber())
-        .pipe(sourcemaps.init())
-        .pipe(sass.sync().on('error', sass.logError))
-        .pipe(postcss(processors)) // px-to-rem & autoprefixer
-        // .pipe(nano({ safe: true, autoprefixer: false, discardComments: { removeAll: false }, zindex: false }))
-        .pipe(nano({ safe: true, autoprefixer: false, zindex: false }))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(skinPathMD))
-        .pipe(browserSync.stream({ match: '**/*.css' }));
-});
-
 /*******************************************************************************
  Watch files for changes & reload
 *******************************************************************************/
-// HNK Split
 gulp.task('serve:hnk', ['styles:hnk'], function () {
   browserSync.init({
         injectChanges: true,
         proxy: "http://hnksplit.dev.sistemi.hr"
     });
     gulp.watch([skinPathHNK + '/styles/**/*.{scss,css}'], ['styles:hnk']);
-});
-
-// Splitsko ljeto
-gulp.task('serve:sl', ['styles:sl'], function () {
-  browserSync.init({
-        injectChanges: true,
-        proxy: "http://splitsko-ljeto.dev.sistemi.hr"
-    });
-    gulp.watch([skinPathSL + '/styles/**/*.{scss,css}'], ['styles:sl']);
-});
-
-// Marulicevi dani
-gulp.task('serve:md', ['styles:md'], function () {
-  browserSync.init({
-        injectChanges: true,
-        proxy: "http://marulicevi-dani.dev.sistemi.hr"
-    });
-    gulp.watch([skinPathMD + '/styles/**/*.{scss,css}'], ['styles:md']);
 });
