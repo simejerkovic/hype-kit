3bf5d3420e32e709b54d5d4e396822c523f53172
Šime Jerković <sime@sistemi.hr>
2017-08-21 14:37:00 +0200

    New functions for Typography .. and more
    Added new functions for typography and color properties, new maps for
    font, color, z -index, removed deprecated components and styles

--- gulpfile.js
+++ gulpfile.js
@@ -29,6 +29,9 @@ var sassdoc = require('sassdoc'); // Sass documentation
 
 var skinPathDev = 'app';
 
+// Settings for Autoprefixer plugin,
+// adds basic browser support + ios 7+ for older iPads
+// ==========================================================================
 var processors = [
   autoprefixer({ browsers: ['> 1%', 'last 2 versions', 'ie > 6', 'ff ESR', 'bb >= 7', 'ios >= 7'] }),
 ];
@@ -36,27 +39,29 @@ var processors = [
 // TODO: Make const paths
 // ==========================================================================
 
-const paths = {
-    ascx: './source/views/*.ascx',
-    js: './source/assets/javascripts/**/*.js',
-    scss: './source/assets/stylesheets/**/*.scss',
-    images: './source/assets/images/*',
-    fonts: './source/assets/fonts/*',
-    //
-    skin: 'app'
-};
+// const paths = {
+//     ascx: './source/views/*.ascx',
+//     js: './source/assets/javascripts/**/*.js',
+//     scss: './source/assets/stylesheets/**/*.scss',
+//     images: './source/assets/images/*',
+//     fonts: './source/assets/fonts/*',
+//     //
+//     skin: 'app'
+// };
 
 // Minify PNG, JPEG, GIF and SVG images with imagemin
 // ==========================================================================
-gulp.task('minify-all-images', () =>
-	gulp.src('Website/Portals/0/Images/**/*')
-		.pipe(imagemin())
-		.pipe(gulp.dest('Website/Portals/0/dist/images'))
-);
+// gulp.task('minify-all-images', () =>
+// 	gulp.src('Website/Portals/0/Images/**/*')
+// 		.pipe(imagemin())
+// 		.pipe(gulp.dest('Website/Portals/0/dist/images'))
+// );
 
 // Compile Sass to CSS
 // ==========================================================================
 
+// Use only for Development environment!
+// For production see task styles:prod
 gulp.task('styles:dev', function () {
 
     return gulp.src(skinPathDev + '/styles/skin.scss')
@@ -71,6 +76,8 @@ gulp.task('styles:dev', function () {
         .pipe(browserSync.stream({ match: '**/*.css' }));
 });
 
+// Use only for Production / Live environment!
+// For development see task styles:dev
 gulp.task('styles:prod', function () {
 
     return gulp.src(skinPathDev + '/styles/skin.scss')
